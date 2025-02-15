cd2e78b42a1513f60dd742fdb6c168811b2a2319
Adetunji Adeleye <igbengoos@hotmail.com>
2025-02-15 15:53:45 +0000

    NEw NEw
    Just messing arrround with the new keyboard

---
+++ NewDev/React-Jobs/test-mode/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='header--one'>
+        <h1>Welcome to my first react page</h1>
+        <p>This where the programming beggings</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
