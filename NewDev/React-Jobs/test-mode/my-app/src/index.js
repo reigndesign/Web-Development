7b0a0091a875989a9566355db25fe1e40846d445
Adetunji Adeleye <igbengoos@hotmail.com>
2025-02-23 18:17:51 +0000

    all idle code

--- NewDev/React-Jobs/test-mode/my-app/src/index.js
+++ NewDev/React-Jobs/test-mode/my-app/src/index.js
@@ -1,17 +1,128 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "images/pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "images/pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "images/pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "images/pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "images/pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "images/pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+        <Menu />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+function Header() {
+  const style = { color: "red", fonSize: "48px", textTranform: "uppercase" };
+
+  return (
+    <>
+      <header className="header">
+        <h1 style={style}>Fast Reat Pizza Co.</h1>
+      </header>
+    </>
+  );
+}
+
+function Menu() {
+  return (
+    <>
+      <main className="menu">
+        <h2>Our Menu</h2>
+      </main>
+      <Pizzas
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="images/pizzas/spinaci.jpg"
+        price={20}
+      />
+      <Pizzas
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="images/pizzas/funghi.jpg"
+        price={50}
+      />
+    </>
+  );
+}
+
+function Pizzas(props) {
+  return (
+    <>
+      <div className="pizza">
+        <img src={props.photoName} alt={props.name} />
+        <div>
+          <h1>{props.name}</h1>
+          <p>{props.ingredients}</p>
+          <span>{props.price}</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
