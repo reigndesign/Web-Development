c8bdb7036284a9222b9b8936245de55f862e1e38
Adetunji Adeleye <igbengoos@hotmail.com>
2025-04-15 02:54:29 -0700

    From Old to New

--- HNG/FrontEnd/react--app/event-app/src/components/button.jsx
+++
@@ -1,7 +0,0 @@
-const button = () => {
-  return (
-    <div>button</div>
-  )
-}
-
-export default button
--- README.md
+++
@@ -1,2 +0,0 @@
-# Web-Development
-This is where all those folder would be living -2025 Febuary 01 5:30PM
