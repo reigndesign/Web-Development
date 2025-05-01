35137291a4d460b8854ed02c91934ef4b90228ee
Adetunji Adeleye <igbengoos@hotmail.com>
2025-05-01 13:06:01 -0700

    Next Morning

--- FrontEnd/SuperBet/src/carousel.js
+++ FrontEnd/SuperBet/src/carousel.js
@@ -1,7 +1,7 @@
 export default function CarouselHomePage() {
   return (
     <>
-    
+      
     </>
   )
 }
--- FrontEnd/SuperBet/src/navbar.js
+++ FrontEnd/SuperBet/src/navbar.js
@@ -1,12 +1,14 @@
+import mainLogo from './img/logo.jpeg';
+
 export default function NavBar() {
   return (
     <>
       <main className="container">
         <nav className="main--nav">
           <div className="nav--data">
-            <img src="./logo.png" alt="main--logo" className="main--logo" />
-            <span><a href="##"><img src="./order.png"/></a></span>
-            <span><a href="##"><img src="./order.png"/></a></span>
+            <img src={ mainLogo } alt="main--logo" className="main--logo" />
+            <span><a href="##">Sports</a></span>
+            <span><a href="##">Casino</a></span>
           </div>
           <ul className="component--data">
               <li className="list-item"><a href="##">Demo</a></li>
