240c277027aadb8e1d048ea68598a9055e0cf4ac
Adetunji Adeleye <igbengoos@hotmail.com>
2025-05-09 11:44:35 -0700

    Resource added

--- FrontEnd/SuperBet/src/lastestGames.js
+++ FrontEnd/SuperBet/src/lastestGames.js
@@ -1,3 +1,16 @@
+import User101 from "./img/user--01.jpeg";
+import User201 from "./img/user--02.jpeg";
+import User301 from "./img/dynamic-hero-header-for-superbet--02.jpeg";
+
+const LatestGamesData = [
+  {
+    name: "BasketBall",
+    dicr: "",
+    photoname: "./img/dynamic-hero-header-for-superbet--04.jpeg",
+},
+
+];
+
 export default function LatestGames() {
   return (
     <>
@@ -5,17 +18,17 @@ export default function LatestGames() {
         <h3 className="latest--games--title">Latest Games</h3>
         <div className="latest--games--container">
           <div className="latest--games--card">
-            <img src="https://example.com/game1.jpg" alt="Game 1" className="latest--image" />
+            <img src={ User101 } alt="Game 1" className="latest--image" />
             <h2 className="latest--games-title">Game 1</h2>
             <p className="latest--games-description">Description of Game 1.</p>
           </div>
           <div className="latest--games--card">
-            <img src="https://example.com/game2.jpg" alt="Game 2" className="latest--image" />
+            <img src={ User201 } alt="Game 2" className="latest--image" />
             <h2 className="latest--games-title">Game 2</h2>
             <p className="latest--games-description">Description of Game 2.</p>
           </div>
           <div className="latest--games--card">
-            <img src="https://example.com/game3.jpg" alt="Game 3" className="latest--image" />
+            <img src={ User301 } alt="Game 3" className="latest--image" />
             <h2 className="latest--games-title">Game 3</h2>
             <p className="latest--games-description">Description of Game 3.</p>
           </div>
