952fa1c5be88547f7286c36d025b103a5ce0972b
Adetunji Adeleye <igbengoos@hotmail.com>
2025-10-12 01:01:22 +0100

    Added new images to the repo
    Hi Hi

--- FrontEnd/WarriSports/home/src/Components/sports.js
+++ FrontEnd/WarriSports/home/src/Components/sports.js
@@ -19,15 +19,15 @@ function Sports() {
 
   return (
     <div className="ws-container">
-      <div className="grid grid-cols-1 xl:grid-cols-4 gap-4 px-4 py-3">
-        <div className={`ws-sidebar bg-emerald-500 ${isOpen ? "w-72" : "w-20"} w-72 h-screen rounded-lg shadow-md relative`}>
-          <BsArrowLeftCircle className='bg-white text-emerald-700 rounded-full text-3xl absolute -right-3 top-9 border border-emerald-300 cursor-pointer' onClick={() => setIsOpen(!isOpen)} />
+      <div className="flex justify-between items-start gap-5">
+        <div className={`ws-sidebar z-[100] mt-5 bg-emerald-500 ${isOpen ? "w-50" : "w-12"} p-5 pt-8 h-screen rounded-lg shadow-md relative`}>
+          <BsArrowLeftCircle className="bg-white text-emerald-700 rounded-full text-3xl absolute -right-3 top-9 border border-emerald-300 cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
           <h1>One</h1>
         </div>
-        <div className="ws-middle w-full h-24 bg-[#484849] mt-5 rounded-sm text-white">
+        <div className="ws-middle w-8/12 h-24 bg-[#484849] mt-5 rounded-sm text-white">
           <SportsCarousel />
         </div>
-        <div className="ws-left-sidebar bg-emerald-600 w-32 h-24 mt-5">
+        <div className="ws-left-sidebar bg-emerald-600 w-50 h-24 mt-5 mr-5 ">
           <h1>Three</h1>
         </div>
       </div>
@@ -36,22 +36,24 @@ function Sports() {
 }
 
 function SportsCarousel() {
+
+  return (
+
     <Swiper
       modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
       spaceBetween={100}
       autoplay={{ delay: 2500, disableOnInteraction: false }}
       slidesPerView={2}
       onSlideChange={() => {}}
-      onSwiper={(swiper) => {}}
-     
+      onSwiper={(swiper) => {}}>
 
-    >
       <SwiperSlide>Slide 1</SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
       <SwiperSlide>Slide 4</SwiperSlide>
 
     </Swiper>
+  )
 };
 
 export default Sports
